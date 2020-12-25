f0d7cd8565e9bc9082c1c6da38d95904c860cf24
DenisRafi <62852838+DenisRafi@users.noreply.github.com>
2020-12-25 02:58:07 +0000

    Add files via upload

---
+++ .cs
@@ -0,0 +1,14 @@
+using static System.Console;
+
+namespace _
+{
+    class _
+    {
+        static void Main()
+        {
+            Title = "DR";
+            WriteLine("To all of you,\nHappy Holidays\n&\nBest Wishes.");
+            ReadKey();
+        }
+    }
+}
